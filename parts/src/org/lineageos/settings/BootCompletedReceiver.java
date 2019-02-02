af19a878c9cdd4de246022014f52125ec7d66007
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-02-02 14:31:51 -0200

    sdm660-common: dirac: Only call setEnabled on boot completed
    Change-Id: I2dea2ec477b2728e8706d6c30c60f9fc8faa9c95

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -40,6 +40,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         DozeUtils.checkDozeService(context);
         ThermalUtils.startService(context);
-        new DiracUtils(context);
+        new DiracUtils(context).onBootCompleted();
     }
 }
